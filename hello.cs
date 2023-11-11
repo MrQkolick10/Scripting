c3a2a2de2c94af1b54f8277a50e7816107a6a9f3
MrQkolick10 <150513697+MrQkolick10@users.noreply.github.com>
2023-11-11 15:06:06 +0600

    Add files via upload

---
+++ hello.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hello : MonoBehaviour
+{
+    
+    void Start()
+    {
+     print ("Hello world");
+    }
+
+}
